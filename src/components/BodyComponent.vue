e39989da24207c794996c41a30dc572c3c60b1a3
karol <karolrds01@gmail.com>
2023-11-17 00:12:18 -0400

    a

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,3 +1,93 @@
 <script>
-    export default {}
+    export default {
+        data(){
+            return{
+                ingredients: ["Alho", "Orégano", "Manteiga"]
+            }
+        }
+    }
 </script>
+
+<template>
+    <main class="principal-content">
+        <section>
+            <span class="subtitle-lg your-list-text">
+                Sua lista:
+            </span>
+        </section>
+
+        <ul v-if="ingredients.length" class="your-list-ingredients">
+            <li v-for="ingredient in ingredients" :key="ingredient" class="ingredients">{{ ingredient }}</li>
+        </ul>
+
+        <p v-else class="paragraph empty-list">
+          <img src="../images/icones/lista-vazia.svg" alt="">
+          Sua lista está vazia, selecione ingredientes para iniciar.
+        </p>
+    </main>
+</template>
+
+<style scoped>
+.principal-content {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.your-list-text {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.your-list-ingredients{
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.ingredients {
+  display: inline-block;
+  border-radius: 0.5rem;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+    transition: 0.2s;
+    color: var(--creme, #FFFAF3);
+  background: var(--coral, #F0633C);
+  font-weight: 700;
+}
+
+.empty-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .principal-content {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .principal-content {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
